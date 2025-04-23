e1260d61e35ec03e94a869a08155c065dfca2433
eliskleen <eliskleen00@gmail.com>
2025-04-23 08:57:42 +0200

    Added simple replaty stack calculator

---
+++ inc/bench/impl/stackImpl/StackReplayImp.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include "bench/util/Executor.hpp"
+#include "bench/util/InputData.hpp"
+#include <cstddef>
+
+namespace bench {
+    class StackReplayImp : public AccurateStackExecutor {
+      public:
+        StackReplayImp() = default;
+        ~StackReplayImp() = default;
+
+        AbstractExecutor::Measurement calcMaxMeanError() override;
+        void prepare(const InputData &data) override;
+        AbstractExecutor::Measurement execute() override;
+        void reset() override;
+
+      private:
+        struct Event {
+            uint64_t time;
+            uint64_t value;
+            bool isPut;
+        };
+        struct StackItem {
+            uint64_t value;
+            StackItem *next;
+        };
+        std::vector<Event> events; 
+        StackItem *stackHead;
+        size_t stackSize;
+        size_t get_stamps_size;
+    };
+}
---
+++ src/impl/stackImpl/StackReplayImp.cpp
@@ -0,0 +1,81 @@
+#include "bench/impl/stackImpl/StackReplayImp.hpp"
+#include "bench/util/InputData.hpp"
+#include <algorithm>
+#include <cstdint>
+#include <format>
+#include <sys/types.h>
+
+namespace bench {  
+    AbstractExecutor::Measurement StackReplayImp::calcMaxMeanError() {
+        return AbstractExecutor::Measurement();
+    }
+
+
+    AbstractExecutor::Measurement StackReplayImp::execute() {
+        uint64_t max = 0;
+        uint64_t sum = 0;
+        for(const auto &event : events) {
+            uint64_t rank = 0;
+            uint64_t key = event.value;
+            if (event.isPut) {
+                StackItem *newItem = new StackItem{event.value, stackHead};
+                stackHead = newItem;
+                stackSize++;
+            } else {
+                if(stackHead->value == key) {
+                    rank = 0;
+                    //is this a memory leak?
+                    stackHead = stackHead->next;
+                }
+                else{
+                    rank = 1;
+                    StackItem *current = stackHead;
+                    while (current->next->value != key) {
+                        current = current->next;
+                        rank += 1;
+                        if (current->next == nullptr) {
+                            throw std::runtime_error(
+                                std::format("Put for elem {} not found\n", key));
+                        }
+                    }
+                    // current->next has the removed item, so just unlink it from the
+                    // data structure
+                    current->next = current->next->next;
+                }
+                sum += rank;
+                if (rank > max)
+                    max = rank;
+            }
+        }
+        return {max, (long double)sum / (long double)get_stamps_size};
+    }
+
+    void StackReplayImp::prepare(const InputData &data) {
+        auto puts = data.getPuts();
+        auto gets = data.getGets();
+        get_stamps_size = gets->size();
+        events.reserve(puts->size() + gets->size());
+        for (const auto &put : *puts) {
+            events.push_back({put.time, put.value, true});
+        }
+        for (const auto &get : *gets) {
+            events.push_back({get.time, get.value, false});
+        }
+        // Sort events by time 
+        std::sort(events.begin(), events.end(),
+                  [](const Event &a, const Event &b) { return a.time < b.time; });
+        stackHead = nullptr;
+        stackSize = 0;
+    }
+
+    void StackReplayImp::reset() {
+        while(stackHead) {
+            StackItem *temp = stackHead;
+            stackHead = stackHead->next;
+            delete temp;
+        }
+        stackSize = 0;
+        events.clear();
+        get_stamps_size = 0;
+    }
+}
