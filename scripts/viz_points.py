37164e601eae5d5d010de213110f8d29cf318371
eliskleen <eliskleen00@gmail.com>
2025-05-15 16:27:00 +0200

    added way to shoew maximum outlier

--- scripts/viz_points.py
+++ scripts/viz_points.py
@@ -5,7 +5,10 @@ import numpy as np
 # folder = 'data/timestamps/FAKE/'
 # folder = 'data/timestamps/2dd-queue-opt-1ms/'
 # folder = 'data/timestamps/dcbo-w32-n1-d1/'
-folder = 'data/benchData/2ddqopt-w64-l32-i4096-n2-d10/'
+# folder = 'data/benchData/2ddqopt-w64-l32-i4096-n2-d10/'
+
+folder = 'data/benchData/2ddq-w8-l4-i64-n12-d200/'
+# folder = 'data/timestamps/2dd-q-law/'
 
 # folder = 'data/timestamps/2dd-queue-opt-500ms/'
 xs = pd.read_csv(
@@ -36,6 +39,22 @@ points = pd.merge(
 # Create scatter plot
 plt.figure(figsize=(8, 6))  # Set the figure size
 plt.scatter(points['timestamp_start'], points['timestamp_end'], c='blue', alpha=0.6, edgecolors='black', s=50, label='Points')
+
+
+
+#show where max rank is, need manual input from calculation algorithm.
+max_id = 894470
+point = points[points['id'] == max_id]
+plt.scatter(point['timestamp_start'], point['timestamp_end'], c='red', alpha=0.6, edgecolors='black', s=50, label='Max ID Point')
+#add line from (0,0) to (max, max) on y and x axis going through the point
+point_x = point['timestamp_start'].values[0]
+point_y = point['timestamp_end'].values[0]
+# plt.plot([0, point_y], [point_x, point_y], color='red', linestyle='--', label='Line to Max ID Point horizontal')
+rankers = (points[points['timestamp_start'] < point_x])[points['timestamp_end'] > point_y]
+plt.scatter(rankers['timestamp_start'], rankers['timestamp_end'], c='green', alpha=0.6, edgecolors='black', s=50, label='Rankers')
+print(f'Rank of point with id {max_id} is {len(rankers)}')
+
+
 
 # Customize plot
 # if folder contains "2dd"
