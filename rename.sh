ce631bda2bc2fa0aa341b3b19cd04cc532d372c3
v-olin <victor.vieweg@icloud.com>
2025-05-26 16:14:47 +0200

    Add renaming to rename script (??)

--- rename.sh
+++ rename.sh
@@ -8,6 +8,22 @@ cp -r ../benchmark-data/correct-data/cut-files/timestamps-2dd-128-64 data/benchD
 cp -r ../benchmark-data/correct-data/cut-files/timestamps-2dd-256-128 data/benchData/2ddq-w256-l128-i65536-n64-d1
 cp -r ../benchmark-data/correct-data/cut-files/timestamps-2dd-512-256 data/benchData/2ddq-w512-l256-i262144-n64-d1
 
+mv data/benchData/2ddq-w8-l4-i64-n64-d1/combined_get_stamps_cut.txt data/benchData/2ddq-w8-l4-i64-n64-d1/combined_get_stamps.txt
+mv data/benchData/2ddq-w16-l8-i256-n64-d1/combined_get_stamps_cut.txt data/benchData/2ddq-w16-l8-i256-n64-d1/combined_get_stamps.txt
+mv data/benchData/2ddq-w32-l16-i1024-n64-d1/combined_get_stamps_cut.txt data/benchData/2ddq-w32-l16-i1024-n64-d1/combined_get_stamps.txt
+mv data/benchData/2ddq-w64-l32-i4096-n64-d1/combined_get_stamps_cut.txt data/benchData/2ddq-w64-l32-i4096-n64-d1/combined_get_stamps.txt
+mv data/benchData/2ddq-w128-l64-i16384-n64-d1/combined_get_stamps_cut.txt data/benchData/2ddq-w128-l64-i16384-n64-d1/combined_get_stamps.txt
+mv data/benchData/2ddq-w256-l128-i65536-n64-d1/combined_get_stamps_cut.txt data/benchData/2ddq-w256-l128-i65536-n64-d1/combined_get_stamps.txt
+mv data/benchData/2ddq-w512-l256-i262144-n64-d1/combined_get_stamps_cut.txt data/benchData/2ddq-w512-l256-i262144-n64-d1/combined_get_stamps.txt
+
+mv data/benchData/2ddq-w8-l4-i64-n64-d1/combined_put_stamps_cut.txt data/benchData/2ddq-w8-l4-i64-n64-d1/combined_put_stamps.txt
+mv data/benchData/2ddq-w16-l8-i256-n64-d1/combined_put_stamps_cut.txt data/benchData/2ddq-w16-l8-i256-n64-d1/combined_put_stamps.txt
+mv data/benchData/2ddq-w32-l16-i1024-n64-d1/combined_put_stamps_cut.txt data/benchData/2ddq-w32-l16-i1024-n64-d1/combined_put_stamps.txt
+mv data/benchData/2ddq-w64-l32-i4096-n64-d1/combined_put_stamps_cut.txt data/benchData/2ddq-w64-l32-i4096-n64-d1/combined_put_stamps.txt
+mv data/benchData/2ddq-w128-l64-i16384-n64-d1/combined_put_stamps_cut.txt data/benchData/2ddq-w128-l64-i16384-n64-d1/combined_put_stamps.txt
+mv data/benchData/2ddq-w256-l128-i65536-n64-d1/combined_put_stamps_cut.txt data/benchData/2ddq-w256-l128-i65536-n64-d1/combined_put_stamps.txt
+mv data/benchData/2ddq-w512-l256-i262144-n64-d1/combined_put_stamps_cut.txt data/benchData/2ddq-w512-l256-i262144-n64-d1/combined_put_stamps.txt
+
 cp -r ../benchmark-data/correct-data/cut-files/dcbo-8 data/benchData/dcbo-w8-i32-n64-d1
 cp -r ../benchmark-data/correct-data/cut-files/dcbo-32 data/benchData/dcbo-w32-i128-n64-d1
 cp -r ../benchmark-data/correct-data/cut-files/dcbo-128 data/benchData/dcbo-w128-i512-n64-d1
@@ -16,3 +32,21 @@ cp -r ../benchmark-data/correct-data/cut-files/dcbo-2048 data/benchData/dcbo-w20
 cp -r ../benchmark-data/correct-data/cut-files/dcbo-8192 data/benchData/dcbo-w8192-i32768-n64-d1
 cp -r ../benchmark-data/correct-data/cut-files/dcbo-32768 data/benchData/dcbo-w32768-i131072-n64-d1
 cp -r ../benchmark-data/correct-data/cut-files/dcbo-65536 data/benchData/dcbo-w65536-i262144-n64-d1
+
+mv data/benchData/dcbo-w8-i32-n64-d1/combined_get_stamps_cut.txt data/benchData/dcbo-w8-i32-n64-d1/combined_get_stamps.txt
+mv data/benchData/dcbo-w32-i128-n64-d1/combined_get_stamps_cut.txt data/benchData/dcbo-w32-i128-n64-d1/combined_get_stamps.txt
+mv data/benchData/dcbo-w128-i512-n64-d1/combined_get_stamps_cut.txt data/benchData/dcbo-w128-i512-n64-d1/combined_get_stamps.txt
+mv data/benchData/dcbo-w512-i2048-n64-d1/combined_get_stamps_cut.txt data/benchData/dcbo-w512-i2048-n64-d1/combined_get_stamps.txt
+mv data/benchData/dcbo-w2048-i8192-n64-d1/combined_get_stamps_cut.txt data/benchData/dcbo-w2048-i8192-n64-d1/combined_get_stamps.txt
+mv data/benchData/dcbo-w8192-i32768-n64-d1/combined_get_stamps_cut.txt data/benchData/dcbo-w8192-i32768-n64-d1/combined_get_stamps.txt
+mv data/benchData/dcbo-w32768-i131072-n64-d1/combined_get_stamps_cut.txt data/benchData/dcbo-w32768-i131072-n64-d1/combined_get_stamps.txt
+mv data/benchData/dcbo-w65536-i262144-n64-d1/combined_get_stamps_cut.txt data/benchData/dcbo-w65536-i262144-n64-d1/combined_get_stamps.txt
+
+mv data/benchData/dcbo-w8-i32-n64-d1/combined_put_stamps_cut.txt data/benchData/dcbo-w8-i32-n64-d1/combined_put_stamps.txt
+mv data/benchData/dcbo-w32-i128-n64-d1/combined_put_stamps_cut.txt data/benchData/dcbo-w32-i128-n64-d1/combined_put_stamps.txt
+mv data/benchData/dcbo-w128-i512-n64-d1/combined_put_stamps_cut.txt data/benchData/dcbo-w128-i512-n64-d1/combined_put_stamps.txt
+mv data/benchData/dcbo-w512-i2048-n64-d1/combined_put_stamps_cut.txt data/benchData/dcbo-w512-i2048-n64-d1/combined_put_stamps.txt
+mv data/benchData/dcbo-w2048-i8192-n64-d1/combined_put_stamps_cut.txt data/benchData/dcbo-w2048-i8192-n64-d1/combined_put_stamps.txt
+mv data/benchData/dcbo-w8192-i32768-n64-d1/combined_put_stamps_cut.txt data/benchData/dcbo-w8192-i32768-n64-d1/combined_put_stamps.txt
+mv data/benchData/dcbo-w32768-i131072-n64-d1/combined_put_stamps_cut.txt data/benchData/dcbo-w32768-i131072-n64-d1/combined_put_stamps.txt
+mv data/benchData/dcbo-w65536-i262144-n64-d1/combined_put_stamps_cut.txt data/benchData/dcbo-w65536-i262144-n64-d1/combined_put_stamps.txt
